3b1529088d81aefad9464470f10bfc8fd811472d
LuchoDx <lucianocod4@gmail.com>
2022-10-18 20:29:55 -0300

    haciendo cambios
    haciendo cambios

---
+++ script.js
@@ -0,0 +1,9 @@
+function myFun1() {
+    document.getElementById("total-a-pagar").innerHTML("hola");
+    }
+
+document.getElementById("boton-tickets-borrar").onclick = function(){
+   
+    myFun1();
+    
+}
