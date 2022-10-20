fc0985b11223f9215301dfad58855ec163f6fa4b
LuchoDx <lucianocod4@gmail.com>
2022-10-20 00:12:41 -0300

    agregando funcionalidad a los botones
    el boton resume hace lo que tiene que hacer. el de borrar aun no

--- script.js
+++ script.js
@@ -1,18 +1,34 @@
 function hacerCalculos(){
     const valorTicket = 200;
-     let selectCan = parseInt(document.getElementById("cantidad").value); 
-     
+    let selectCan = document.getElementById("cantidad").value;   
     let select = document.getElementById("seleccion"); 
     let selectedOption = select.options[select.selectedIndex].value;
 
     if(selectedOption == "estudiante"){
-        let resultado = valorTicket * selectCan;
+        let resultado = valorTicket * selectCan - ((valorTicket * selectCan) * 80 / 100);
         document.getElementById("total-a-pagar").innerHTML = "Total a Pagar: $" + resultado;
-    }
-} 
- 
-document.getElementById("boton-tickets-resumen").onclick = function(){
-   
-    hacerCalculos();
+    } else if(selectedOption == "trainee"){
+        let resultado = valorTicket * selectCan - ((valorTicket * selectCan) * 50 / 100);
+        document.getElementById("total-a-pagar").innerHTML = "Total a Pagar: $" + resultado;
+    } else if(selectedOption == "junior"){
+            let resultado = valorTicket * selectCan - ((valorTicket * selectCan) * 15 / 100);
+            document.getElementById("total-a-pagar").innerHTML = "Total a Pagar: $" + resultado;
+        }
     
+} 
+
+function borrar(){
+
+    document.getElementById("total-a-pagar").innerHTML = "Total a Pagar: $";
+
+}
+
+/*Agrega evento onclick al boton borrar*/
+document.getElementById("boton-tickets-borrar").onclick = function(){ 
+    borrar(); 
+}
+
+ /*Agrega evento onclick al boton resumen*/
+document.getElementById("boton-tickets-resumen").onclick = function(){ 
+    hacerCalculos(); 
 }
